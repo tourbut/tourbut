db187c267524d52e27b6a9860c35b679386e3b0e
Shin <scshin88@gmail.com>
2022-02-23 16:11:44 +0900

    settings.py environ 적용

--- tourbut/settings.py
+++ tourbut/settings.py
@@ -12,20 +12,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-import environ
-import os
+import environ,os
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
