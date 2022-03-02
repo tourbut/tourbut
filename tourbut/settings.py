0b96e80d3f78365eae771d8e31a89ec04a3f8355
Shin <scshin88@gmail.com>
2022-03-02 22:29:17 +0900

    22강( LoginView, LogoutView를 기반으로 로그인 및 로그아웃 기능)

--- tourbut/settings.py
+++ tourbut/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import environ,os
+from django.urls import reverse_lazy
 
 env = environ.Env(
     # set casting, default value
@@ -136,3 +137,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
