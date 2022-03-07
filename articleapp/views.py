62a9854e85df4eb36ae9fdbd23f7fa95ad2e3e7c
Shin <scshin88@gmail.com>
2022-03-07 21:11:37 +0900

    36강(Aricle앱 list View, List 꾸미기)

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_requried
 from articleapp.forms import ArticleCreationForm
@@ -47,8 +47,16 @@ class ArticleUpdateView(UpdateView):
         return reverse('articleapp:detail',kwargs={'pk': self.object.pk})
 
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class ArticleDeleteView(DeleteView):
     model=Article
     context_object_name = 'target_article'
     template_name = 'articleapp/delete.html'
     success_url = reverse_lazy('articleapp:list')
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 2
