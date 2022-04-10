db20ced4e40f998cb9072dd55a6d38a8bb8b0298
Shin <scshin88@gmail.com>
2022-04-10 22:54:16 +0900

    43강(Field Lookup 사용으로 구독앱 수정)

--- subscribapp/urls.py
+++ subscribapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from projectapp.views import ProjectListView, ProjectCreateView, ProjectDetailView
-from subscribapp.views import SubscriptionView
+from subscribapp.views import SubscriptionView, SubscriptionListView
 
 app_name = "subscribapp"
 
 urlpatterns = [
     path('subscribe/', SubscriptionView.as_view(),name='subscribe'),
+    path('list/', SubscriptionListView.as_view(),name='list'),
 ]
